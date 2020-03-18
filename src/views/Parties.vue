0673c3230fc000cbf2f99546c6bf45ed843c2bbf
Drake Foltz <GeenDutchman@byu.net>
2020-03-18 10:06:05 -0600

    hooked up link

--- src/views/Parties.vue
+++ src/views/Parties.vue
@@ -7,7 +7,7 @@
             <!-- <img src="../assets/logo.png"> -->
             <party-card src="/assets/logo.png" v-bind:party_id="-1" link="/account" party_name="Sign Up!" party_description="Sign up and make your own party!"  v-bind:party_members="[]"/>
             <div v-for="party in this.$root.$data.parties" :key="party.party_id">
-                <party-card :src="(party.party_image ? party.party_image : '/assets/logo-bw.svg')" :party_id="party.party_id" :party_name="party.party_name" :party_description="party.party_description" :party_members="party.party_members" :link="(party.party_page ? party.party_page : '/')" />
+                <party-card :src="(party.party_image ? party.party_image : '/assets/logo-bw.svg')" :party_id="party.party_id" :party_name="party.party_name" :party_description="party.party_description" :party_members="party.party_members" :link="(party.party_page ? party.party_page : '/parties/' + party.party_id)" />
             </div>
         </div>
     </div>
