027ebc0bda6ad2ffdbeb4eba7029275466160be0
Drake Foltz <GeenDutchman@byu.net>
2020-03-18 19:03:55 -0600

    fixed styling
    Added secret link

--- src/views/PartyBlog.vue
+++ src/views/PartyBlog.vue
@@ -6,10 +6,10 @@
         <hr />
         <div v-for="entry in blogEntries" :key="entry.entry_id" :class="[ 'card', entry.author_script,  'section', 'blogItem', 'color-primary-4']" >
             <p>{{entry.text}}</p>
-            <p class="right">--<span v-text="(entry.date ? entry.date  + '-': '')"></span><span>{{entry.author_name}}</span></p>
+            <p class="signature">——{{(entry.date ? entry.date  + '——': '')}}{{entry.author_name}}</p>
         </div>
         <div class="section center">
-            <p>There are no earlier posts</p>
+            <router-link to='/' tag="p">There are no earlier posts</router-link>
         </div>
     </div>
 </template>
@@ -29,10 +29,11 @@ export default {
 </script>
 
 <style scoped>
-.right {
+.signature {
     display: flex;
     justify-items: end;
     margin-left: auto;
+    text-align: end;
 }
 
 .center {
@@ -44,7 +45,7 @@ export default {
     display: flex;
     justify-content: space-around;
     max-width: 70%;
-    min-width: 60rem;
+    min-width: min-content;
     align-self: center;
     padding: 1rem;
     margin: 1rem;
--- src/views/PartyIntro.vue
+++ src/views/PartyIntro.vue
@@ -2,13 +2,17 @@
     <div class="full">
         <div class="section">
             <h1>{{party.party_name}}</h1>
-            <p class="wizard-script">This is where we introduce ourselves, and write a little background about us.</p>
+            <p><span class="wizard-script">This is where we introduce ourselves, and write a little background about us. </span></p>
+            <router-link :to="blogRoute" class='btn btn-primary'>See blog!</router-link>
         </div>
         <hr />
         <div v-for="person in party.party_members" :key="person.adventurer_id" :class="['section', person.adventurer_script]">
             <h3>{{person.adventurer_name}}</h3>
             <p>{{person.adventurer_intro}}</p>
             <hr />
+        </div>
+        <div class="section">
+            <router-link :to="blogRoute" class='btn btn-primary'>See blog!</router-link>
         </div>
     </div>
 </template>
@@ -19,7 +23,22 @@ export default {
     computed: {
         party() {
             return this.$root.$data.parties[this.$route.params.id];
+        },
+        blogRoute() {
+            return this.$route.path + '/blog';
         }
     }
 }
 </script>
+
+<style scoped>
+.right {
+    float: right;
+    margin-left: auto;
+}
+
+.btn {
+    display: flex;
+    align-self: center;
+}
+</style>
